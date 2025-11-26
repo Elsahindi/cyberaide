4803f861941b34e9ddea933b60490b094967f074
elsahindi <elsareneehindi@gmail.com>
2025-11-26 09:33:39 +0100

    student microservice

--- students-service/src/main/java/fr/insatoulouse/studentsservice/model/Feedback.java
+++ students-service/src/main/java/fr/insatoulouse/studentsservice/model/Feedback.java
@@ -1,7 +1,20 @@
 package fr.insatoulouse.studentsservice.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Feedback {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(nullable = false)
     private String writerId;
+
+    @Column(nullable = false)
     private String content;
 }
